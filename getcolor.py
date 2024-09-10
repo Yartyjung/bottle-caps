3ff7bde1fdfe2d37e48a65fd3affb5265c6c51ad
yartyjung <nattapat.ems@gmail.com>
2024-09-11 00:40:32 +0700

    rewrite get color function

--- getcolor.py
+++ getcolor.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 vid = cv.VideoCapture(0)
 
-def get_caps_color(vid) -> str :
+def get_caps_color(vid , show_bool : bool) -> str :
     """
     Return caps color 
     Blue : 1 
@@ -10,21 +10,36 @@ def get_caps_color(vid) -> str :
     White : 3
     Yellow : 4
     """
+    blue : list = []
+    color_score = {"blue" : None,
+                   "green" : None,
+                   "white" : None,
+                   "yellow" : None}
     mask_dict : list = [
-        ((None,None,None),(None,None,None),"blue"),
-        ((None,None,None),(None,None,None),"green"),
-        ((None,None,None),(None,None,None),"white"),
-        ((None,None,None),(None,None,None),"yellow"),
+        ((85,95,80  ),(115,255,255),"blue")
+        # ((None,None,None),(None,None,None),"green"),
+        # ((None,None,None),(None,None,None),"white"),
+        # ((None,None,None),(None,None,None),"yellow")
     ]
-    while vid.isOpened():
-        ret, frame = vid.read(0)
-        print(np.shape(frame))
+    while True : 
+        ret, frame = vid.read() 
         cropped_image = frame[0:480,  80:560]
-        hsv_image = cv.cvtColor(cropped_image,cv.COLOR_BGR2HSV) 
-        cv.imshow("frame",hsv_image)
-        if cv.waitKey(1) & 0xFF == ord('0'): 
-            break
-    color : str = None
-    return color
+        hsv_image = cv.cvtColor(cropped_image,cv.COLOR_BGR2HSV)
+        for lower, upper, color in mask_dict :
+            mask = cv.inRange(hsv_image, np.array(lower), np.array(upper))
+            contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+            areas = 0
+            for contour in contours :
+                area = cv.contourArea(contour)
+                areas.append(area)
+            color_score[color] = sum(areas)
+            if show_bool :
+            # filtered_frame = cv.bitwise_and(cropped_image, cropped_image, mask=mask)
+            # cv.imshow("frame",filtered_frame)
+                pass
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                exit()
+        # color : str = None
+        # return color
 
 get_caps_color(vid)
